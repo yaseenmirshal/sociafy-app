4e08c9d22bc62357b0630d9776da6960a7397233
yaseenmirshal <151372491+yaseenmirshal@users.noreply.github.com>
2024-06-11 15:57:21 +0530

    updated

--- app/Profile/page.tsx
+++ app/Profile/page.tsx
@@ -4,7 +4,6 @@ import Link from "next/link";
 import React from "react";
 import "/common.css";
 import Notification from "../Components/Notification";
-
 import { GlobalContext } from "../Components/context/globalContext";
 import instance from "../instence/instence";
 
