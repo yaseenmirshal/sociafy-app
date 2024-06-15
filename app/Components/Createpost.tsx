a01fbaa0d0fe1dffbf9c7c988689f9664cade37b
yaseenmirshal <151372491+yaseenmirshal@users.noreply.github.com>
2024-06-15 10:29:07 +0530

    removed .length

--- app/Components/Createpost.tsx
+++ app/Components/Createpost.tsx
@@ -428,7 +428,7 @@ export const Createpost: React.FC<NotificationProps> = ({
             d="M21 8.25c0-2.485-2.099-4.5-4.688-4.5-1.935 0-3.597 1.126-4.312 2.733-.715-1.607-2.377-2.733-4.313-2.733C5.1 3.75 3 5.765 3 8.25c0 7.22 9 12 9 12s9-4.78 9-12Z"
           />
         </svg>
-        <h1 className="float-left mt-1 mr-6">{item.likes.length}</h1>
+        <h1 className="float-left mt-1 mr-6"></h1>
         
         <svg
           xmlns="http://www.w3.org/2000/svg"
@@ -445,7 +445,7 @@ export const Createpost: React.FC<NotificationProps> = ({
           />
         </svg>
         <h1 style={{ paddingTop: "5px" }} className="float-left">
-          {item?item.comments.length:0}
+          {/* {item?item.comments.length:0} */}
         </h1>
 
         <div className="flex mt-2">
